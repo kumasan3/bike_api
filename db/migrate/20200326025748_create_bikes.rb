377badcdfa9015e376707071c8fe442fc164d822
kumasan3 <kumasan3san@yahoo.co.jp>
2020-03-26 12:03:42 +0900

    bikeテーブル作成

---
+++ db/migrate/20200326025748_create_bikes.rb
@@ -0,0 +1,10 @@
+class CreateBikes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :bikes do |t|
+      t.string :name
+      t.string :string
+
+      t.timestamps
+    end
+  end
+end
