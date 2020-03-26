7b66414fe295f612f0d91c2f83bbfd0a4f0054a9
kumasan3 <kumasan3san@yahoo.co.jp>
2020-03-26 13:27:28 +0900

    Brandテーブルのnameカラムにuniqueの制約追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_26_041412) do
+ActiveRecord::Schema.define(version: 2020_03_26_042500) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_03_26_041412) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_brands_on_name", unique: true
   end
 
 end
