528dabb4a8c1dcfe811a259a69d93ea75ef10a97
kumasan3 <kumasan3san@yahoo.co.jp>
2020-03-28 12:31:01 +0900

    request_spec initial commit

--- spec/request/bike_spec.rb
+++ spec/request/bike_spec.rb
@@ -1,2 +1,15 @@
-# require 'rails_helper'
+require 'rails_helper'
 
+RSpec.describe "Inventory", type: :request do
+    describe "自転車登録 API " do
+        context "リクエストの brand_name が brands テーブルの name に存在しない場合" do
+            it 'brand_nameとserial_numberで自転車情報を登録できること' do
+                post "/bikes", params: {brand_name: "HONDA", serial_number: "12345"}
+                bike = Bike.find_by(serial_number: "12345")
+                expect(response).to have_http_status(200)
+                expect(bike.brand.name).to eq("HONDA")
+                puts response.body
+            end
+        end
+    end
+end
