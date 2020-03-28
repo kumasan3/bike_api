1b20585fd54dd4ac66eca3db90f3892d0edd97a4
kumasan3 <kumasan3san@yahoo.co.jp>
2020-03-28 13:44:01 +0900

    feature spec bike追加

--- spec/request/bike_spec.rb
+++ spec/request/bike_spec.rb
@@ -2,21 +2,49 @@ require 'rails_helper'
 
 RSpec.describe "Inventory", type: :request do
     describe "自転車登録 API " do
+        before do
+            @brand = FactoryBot.create(:brand, name: "SUZUKI")
+            @bike = FactoryBot.create(:bike, brand_id: @brand.id)
+            @count_before = Bike.count
+        end
         context "リクエストの brand_name が brands テーブルの name に存在しない場合" do
             it 'brand_nameとserial_numberで自転車情報を登録できること' do
                 post "/bikes", params: {brand_name: "HONDA", serial_number: "12345"}
-                bike = Bike.find_by(serial_number: "12345")
                 expect(response).to have_http_status(201)
-                expect(bike.brand.name).to eq("HONDA")
+                expect(Bike.count).to eq(@count_before + 1)
             end
             it 'brand_nameが無ければ登録できないこと' do
                 post "/bikes", params: {brand_name: "", serial_number: "12345"}
                 bike = Bike.find_by(serial_number: "12345")
-                puts response.body
                 expect(response).to have_http_status(422)
                 expect(bike.nil?).to eq(true)
+                expect(Bike.count).to eq(@count_before)
             end
-            
+            it 'brand_nameが無ければ登録できないこと #2' do
+                post "/bikes", params: {brand_name: nil, serial_number: "12345"}
+                bike = Bike.find_by(serial_number: "12345")
+                expect(response).to have_http_status(422)
+                expect(bike.nil?).to eq(true)
+                expect(Bike.count).to eq(@count_before)
+            end
+            it 'serial numberが無ければ登録できないこと' do
+                post "/bikes", params: {brand_name: "HONDA", serial_number: ""}
+                expect(@count_before).to eq(Bike.count)
+                expect(response).to have_http_status(422)
+                expect(Bike.count).to eq(@count_before)
+            end
+            it 'serial numberが無ければ登録できないこと #2' do
+                post "/bikes", params: {brand_name: "HONDA", serial_number: nil}
+                expect(@count_before).to eq(Bike.count)
+                expect(response).to have_http_status(422)
+                expect(Bike.count).to eq(@count_before)
+            end
+            it '既に存在するserial numberでは登録できないこと' do
+                post "/bikes", params: {brand_name: "HONDA", serial_number: @bike.serial_number}
+                expect(@count_before).to eq(Bike.count)
+                expect(response).to have_http_status(422)
+                expect(Bike.count).to eq(@count_before)
+            end
 
         end
     end
